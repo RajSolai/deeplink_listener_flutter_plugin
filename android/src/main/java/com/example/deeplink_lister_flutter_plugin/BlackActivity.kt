34ea94bfd4956f44bf0428a5e61fd2611a15a941
RajSolai <msraj085@gmail.com>
2022-10-04 21:56:46 +0530

    added data parsing code to BlackActivity.kt file

--- android/src/main/java/com/example/deeplink_lister_flutter_plugin/BlackActivity.kt
+++ android/src/main/java/com/example/deeplink_lister_flutter_plugin/BlackActivity.kt
@@ -1,11 +1,27 @@
 package com.example.deeplink_lister_flutter_plugin
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class BlackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_black)
+        val uri: Uri? = intent.data
+        if(uri != null){
+            val label:TextView = findViewById(R.id.textView)
+            label.text = uri.getQueryParameter("data")
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        val uri: Uri? = intent.data
+        if(uri != null){
+            val label:TextView = findViewById(R.id.textView)
+            label.text = uri.getQueryParameter("data")
+        }
     }
 }
